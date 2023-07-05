8ba60a190db56c6cb31d951cafc2ddc6564b47ba
Jatmy <jatmyy@gmail.com>
2023-07-05 16:05:03 +0700

    upgrade trait with-relationships

--- app/Traits/WithRelationships.php
+++ app/Traits/WithRelationships.php
@@ -9,6 +9,19 @@ trait WithRelationships
 
     public function scopeWithRelationships($query, array|string $with)
     {
-        return $query->with(array_intersect(Arr::wrap($with), static::$relationships ?? []));
+        $validRelationships = collect($with)
+            ->map(fn(string $relationships) => explode('.', $relationships))
+            ->filter(function ($relationships) {
+                return collect($relationships)->reduce(function ($model, $relationship) {
+                    if($model && method_exists($model, $relationship) && in_array($relationship, $model::$relationships)) {
+                        return $model->$relationship()->getRelated();
+                    }
+                    return null;
+                }, new static);
+            })
+            ->map(fn(array $relationships) => implode('.', $relationships))
+            ->all();
+
+        return $query->with($validRelationships);
     }
 }
